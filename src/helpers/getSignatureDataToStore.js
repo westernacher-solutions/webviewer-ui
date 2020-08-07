450cf9a2659dd8959a916927a9ed80a69fd26c35
Zhijie Zhang <zhijiezhang0124@gmail.com>
2020-08-07 15:35:56 -0700

    fix: race condition in getSignatureDataToStore

--- src/helpers/getSignatureDataToStore.js
+++ src/helpers/getSignatureDataToStore.js
@@ -1,16 +1,19 @@
 import core from 'core';
 
+let previewsPromise = Promise.resolve();
 // returns an array of objects in the shape of: { annotation, preview }
 // annotation: a copy of the annotation passed in
 // imgSrc: preview of the annotation, a base64 string
 export default async annotations => {
+  await previewsPromise;
+
   // copy the annotation because we need to mutate the annotation object later if there're any styles changes
   // and we don't want the original annotation to be mutated as well
   // since it's been added to the canvas
   const signatureTool = core.getTool('AnnotationCreateSignature');
   const newAnnotations = annotations.map(core.getAnnotationCopy);
-  const previews = await Promise.all(
-    annotations.map(annotation => {
+  previewsPromise = Promise.all(
+    newAnnotations.map(annotation => {
       const oldStrokeThickness = annotation['StrokeThickness'];
       // Temporarily changing stroke thickness to 6 because it looks better in the preview image
       annotation['StrokeThickness'] = 6;
@@ -20,6 +23,7 @@ export default async annotations => {
     }),
   );
 
+  const previews = await previewsPromise;
   return newAnnotations.map((annotation, i) => ({
     annotation,
     imgSrc: previews[i],
